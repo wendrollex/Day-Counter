d23ed5ac149caeeb73f0ba4f92ffcb4783731c7d
Wendrollex <147972791+wendrollex@users.noreply.github.com>
2023-10-15 14:45:08 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+
+        const daysEl = document.getElementById("days");
+ const hoursEl = document.getElementById("hours");
+ const minsEl = document.getElementById("mins");
+ const secEl = document.getElementById("sec"); 
+
+ const newYear = '15 October 2023';
+
+  function countdown(){
+      const newYearDate = new Date(newYear);
+      const currentDate = new Date();
+
+      const totalSeconds = (currentDate- newYearDate)/1000;
+
+      const days = Math.floor(totalSeconds/3600/24);
+      const hours = Math.floor(totalSeconds/3600) % 24;
+
+      const mins = Math.floor(totalSeconds/60) % 60;
+      const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML=days;
+     hoursEl.innerHTML=formatTime(hours);
+     minsEl.innerHTML= formatTime(mins);
+     secEl.innerHTML=formatTime(seconds);
+     /* console.log( days, hours, mins, seconds) */
+     
+  }
+  function formatTime(time){
+      if (time < 10) {
+          return ("0"+time)
+      } else {
+          return time
+      }
+  }
+
+  countdown();
+
+  setInterval(countdown, 1000);
